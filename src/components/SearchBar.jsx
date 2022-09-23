d5a006b854ecd38e9d9e650378fd4aeefdf52796
Caio Vinicius <caiooom91@gmail.com>
2022-09-23 20:55:25 -0300

    Requisito 10

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,10 +2,32 @@ import React from 'react';
 
 function SearchBar() {
   return (
-    <input
-      type="text"
-      data-testid="search-input"
-    />
+    <label htmlFor="search">
+      <form name="search">
+
+        <input
+          type="text"
+          data-testid="search-input"
+        />
+        <div className="search-radio-buttons">
+          <input type="radio" data-testid="ingredient-search-radio" />
+          Ingrediente
+
+          <input type="radio" data-testid="name-search-radio" />
+          Nome
+
+          <input type="radio" data-testid="first-letter-search-radio" />
+          Primeira letra
+
+          <button
+            type="button"
+            data-testid="exec-search-btn"
+          >
+            Buscar
+          </button>
+        </div>
+      </form>
+    </label>
   );
 }
 
