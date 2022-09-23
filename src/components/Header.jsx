778e01a4c4fadabb9ce6752001208f6d59332ac5
Caio Vinicius <caiooom91@gmail.com>
2022-09-23 20:41:01 -0300

    Requisitos 8 e 9

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import searchIcon from '../images/searchIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header({ title, iconSearch }) {
+  const history = useHistory();
+  const [showInput, setShowInput] = useState(false);
+
+  const showSearchBar = () => {
+    setShowInput(!showInput);
+  };
+
   return (
     <section>
       <h2 data-testid="page-title">{title}</h2>
       {
         iconSearch
-        && <img data-testid="search-top-btn" src={ searchIcon } alt="Search icon" />
+        && (
+          <button
+            style={ { marginLeft: '5px' } }
+            type="button"
+            onClick={ showSearchBar }
+          >
+            <img
+              data-testid="search-top-btn"
+              src={ searchIcon }
+              alt="Search icon"
+            />
+          </button>
+        )
       }
-      <img data-testid="profile-top-btn" src={ profileIcon } alt="Profile icon" />
+      <button
+        style={ { marginLeft: '5px' } }
+        type="button"
+        onClick={ () => history.push('/profile') }
+      >
+        <img
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          alt="Profile icon"
+        />
+      </button>
+      {
+        showInput
+        && <SearchBar />
+      }
     </section>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <input
+      type="text"
+      data-testid="search-input"
+    />
+  );
+}
+
+export default SearchBar;
