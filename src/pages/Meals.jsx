fb698e984d7bdab4f7d9a62d7ac3dc4426496377
Caio Vinicius <caiooom91@gmail.com>
2022-09-26 17:28:18 -0300

    Redirecionamento para o id da receita quando a requisição só encontra 1 resultado

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import FoodsContext from '../provider/FoodsContext';
 
 function Meals() {
-  const { foods, setSite } = useContext(FoodsContext);
+  const { setSite } = useContext(FoodsContext);
 
   useEffect(() => {
     setSite('themealdb');
--- src/provider/FoodsProvider.js
+++ src/provider/FoodsProvider.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import FoodsContext from './FoodsContext';
 import fetchApi from '../services/fetchApi';
 
 function FoodsProvider({ children }) {
+  const history = useHistory();
   const [foods, setFoods] = useState();
   const [site, setSite] = useState('');
 
   const getFoods = (url) => {
-    fetchApi(url).then((data) => setFoods(data));
+    fetchApi(url).then((data) => {
+      setFoods(data);
+      if (data.meals && data.meals.length === 1) {
+        history.push(`/meals/${data.meals[0].idMeal}`);
+      } if (data.drinks && data.drinks.length === 1) {
+        history.push(`/drinks/${data.drinks[0].idDrink}`);
+      }
+    });
   };
 
   const contextType = {
