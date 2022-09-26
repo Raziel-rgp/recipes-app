d9452b1b53e1842db1ebffc578506256efeff0f0
lucas-da-silva <lucassouza00012345@gmail.com>
2022-09-26 08:24:50 -0300

    refactoring the SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,26 +4,44 @@ function SearchBar() {
   return (
     <label htmlFor="search">
       <form name="search">
-
         <input
           type="text"
           data-testid="search-input"
+          placeholder="Search"
         />
         <div className="search-radio-buttons">
-          <input type="radio" data-testid="ingredient-search-radio" />
-          Ingrediente
-
-          <input type="radio" data-testid="name-search-radio" />
-          Nome
-
-          <input type="radio" data-testid="first-letter-search-radio" />
-          Primeira letra
-
+          <label htmlFor="ingredient-search-radio">
+            <input
+              type="radio"
+              name="searchRadio"
+              data-testid="ingredient-search-radio"
+              id="ingredient-search-radio"
+            />
+            Ingrediente
+          </label>
+          <label htmlFor="name-search-radio">
+            <input
+              type="radio"
+              name="searchRadio"
+              data-testid="name-search-radio"
+              id="name-search-radio"
+            />
+            Nome
+          </label>
+          <label htmlFor="first-letter-search-radio">
+            <input
+              type="radio"
+              name="searchRadio"
+              data-testid="first-letter-search-radio"
+              id="first-letter-search-radio"
+            />
+            Primeira letra
+          </label>
           <button
             type="button"
             data-testid="exec-search-btn"
           >
-            Buscar
+            Search
           </button>
         </div>
       </form>
