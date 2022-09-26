3b6fa12a4aaf8bae920541b3d5d8327ff1c93960
fawl_03 <razidias@gmail.com>
2022-09-26 16:58:39 -0300

    1.1.2 corrigindo lint

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,6 +1,6 @@
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../tests/utils/renderWithRouter';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import Footer from '../components/Footer';
 import Meals from '../pages/Meals';
 
@@ -12,13 +12,13 @@ describe('Tests of Footer', () => {
   });
   test('2- test if on click the button "drinks" the Route is "/drinks"', () => {
     const { history } = renderWithRouter(<Meals />);
-    const button = screen.getByAltText( "drinks" );
+    const button = screen.getByAltText('drinks');
     userEvent.click(button);
     expect(history.location.pathname).toBe('/drinks');
   });
   test('3- test if on click the button "drinks" the Route is "/Meals"', () => {
     const { history } = renderWithRouter(<Meals />);
-    const button = screen.getByAltText( "meals" );
+    const button = screen.getByAltText("meals");
     userEvent.click(button);
     expect(history.location.pathname).toBe('/meals');
   });
