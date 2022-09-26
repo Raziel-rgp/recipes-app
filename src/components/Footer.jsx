164791da853f602a1dca392df63e6e9aa208e993
Renan Pinto <renanrnpinto@gmail.com>
2022-09-26 15:24:47 -0300

    Requisito 16

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import mealIcon from '../images/mealIcon.svg';
+import drinkIcon from '../images/drinkIcon.svg';
+import '../styles/Footer.css';
 
 function Footer() {
+  const history = useHistory();
   return (
-    <div>
-      opa
+    <div
+      data-testid="footer"
+      className="footer"
+    >
+      <button
+        type="button"
+        onClick={ () => history.push('/drinks') }
+      >
+        <img
+          src={ drinkIcon }
+          alt="drinks"
+          data-testid="drinks-bottom-btn"
+        />
+      </button>
+      <button
+        type="button"
+        onClick={ () => history.push('/meals') }
+      >
+        <img
+          src={ mealIcon }
+          alt="meals"
+          data-testid="meals-bottom-btn"
+        />
+      </button>
     </div>
   );
 }
