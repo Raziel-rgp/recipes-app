fe9b9e86c6409b64af19413d14e77d8e02dea70e
lucas-da-silva <lucassouza00012345@gmail.com>
2022-09-27 15:24:54 -0300

    req-20 complete

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -3,16 +3,22 @@ import Header from '../components/Header';
 import FoodsContext from '../provider/FoodsContext';
 import FoodsCards from '../components/FoodsCards';
 import Footer from '../components/Footer';
+import Categories from '../components/Categories';
 
-const MAX_LENGTH_FOODS = 12;
+const MAX_LENGTH_DRINKS = 12;
+const MAX_LENGTH_CATEGORIES = 5;
 
 function Drinks() {
-  const { foods, setSite, getFoods } = useContext(FoodsContext);
+  const {
+    foods, setSite, getFoods, getCategories, categories,
+  } = useContext(FoodsContext);
 
   useEffect(() => {
     const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    const site = 'thecocktaildb';
     getFoods(url);
-    setSite('thecocktaildb');
+    setSite(site);
+    getCategories(site);
   }, []);
 
   if (foods.drinks === null) {
@@ -23,22 +29,38 @@ function Drinks() {
     <section>
       <Header title="Drinks" iconSearch />
       <main className="foods-main-container">
-        {
-          foods.drinks && foods.drinks.length && foods.drinks.map(
-            ({ idDrink, strDrink, strDrinkThumb }, index) => {
-              if (index < MAX_LENGTH_FOODS) {
-                return (<FoodsCards
-                  key={ idDrink }
-                  id={ idDrink }
-                  name={ strDrink }
-                  img={ strDrinkThumb }
-                  index={ index }
+        <div>
+          {
+            categories && categories.drinks
+            && categories.drinks.map(({ strCategory }, index) => {
+              if (index < MAX_LENGTH_CATEGORIES) {
+                return (<Categories
+                  category={ strCategory }
+                  key={ strCategory }
                 />);
               }
               return undefined;
-            },
-          )
-        }
+            })
+          }
+        </div>
+        <div className="foods-main-div">
+          {
+            foods.drinks && foods.drinks.length && foods.drinks.map(
+              ({ idDrink, strDrink, strDrinkThumb }, index) => {
+                if (index < MAX_LENGTH_DRINKS) {
+                  return (<FoodsCards
+                    key={ idDrink }
+                    id={ idDrink }
+                    name={ strDrink }
+                    img={ strDrinkThumb }
+                    index={ index }
+                  />);
+                }
+                return undefined;
+              },
+            )
+          }
+        </div>
       </main>
       <Footer />
     </section>
--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -3,16 +3,22 @@ import FoodsCards from '../components/FoodsCards';
 import Header from '../components/Header';
 import FoodsContext from '../provider/FoodsContext';
 import Footer from '../components/Footer';
+import Categories from '../components/Categories';
 
 const MAX_LENGTH_FOODS = 12;
+const MAX_LENGTH_CATEGORIES = 5;
 
 function Meals() {
-  const { foods, setSite, getFoods } = useContext(FoodsContext);
+  const {
+    foods, setSite, getFoods, getCategories, categories,
+  } = useContext(FoodsContext);
 
   useEffect(() => {
     const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    const site = 'themealdb';
     getFoods(url);
-    setSite('themealdb');
+    setSite(site);
+    getCategories(site);
   }, []);
 
   if (foods.meals === null) {
@@ -23,22 +29,38 @@ function Meals() {
     <section>
       <Header title="Meals" iconSearch />
       <main className="foods-main-container">
-        {
-          foods.meals && foods.meals.length && foods.meals.map(
-            (({ idMeal, strMeal, strMealThumb }, index) => {
-              if (index < MAX_LENGTH_FOODS) {
-                return (<FoodsCards
-                  key={ idMeal }
-                  id={ idMeal }
-                  name={ strMeal }
-                  img={ strMealThumb }
-                  index={ index }
+        <div>
+          {
+            categories && categories.meals
+            && categories.meals.map(({ strCategory }, index) => {
+              if (index < MAX_LENGTH_CATEGORIES) {
+                return (<Categories
+                  category={ strCategory }
+                  key={ strCategory }
                 />);
               }
               return undefined;
-            }),
-          )
-        }
+            })
+          }
+        </div>
+        <div className="foods-main-div">
+          {
+            foods.meals && foods.meals.length && foods.meals.map(
+              (({ idMeal, strMeal, strMealThumb }, index) => {
+                if (index < MAX_LENGTH_FOODS) {
+                  return (<FoodsCards
+                    key={ idMeal }
+                    id={ idMeal }
+                    name={ strMeal }
+                    img={ strMealThumb }
+                    index={ index }
+                  />);
+                }
+                return undefined;
+              }),
+            )
+          }
+        </div>
       </main>
       <Footer />
     </section>
