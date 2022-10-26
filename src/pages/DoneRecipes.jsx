d4c4c879f0c4c4e51fa4f1cbeccb5b0c7991f743
André Melo <proandremelo@gmail.com>
2022-10-26 15:03:00 -0300

    Requisito 44

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getItem } from '../services/LocalStorageFuncs';
+
 import Header from '../components/Header';
 
 function DoneRecipes() {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    setRecipes(getItem('doneRecipes'));
+  }, []);
+
   return (
-    <Header />
+    <section>
+      <Header />
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-meal-btn"
+      >
+        Meals
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+      </button>
+      <ul>
+        {
+          recipes.map((dr, index) => (
+            <li key={ dr.id }>
+              <img
+                src={ dr.image }
+                alt={ dr.name }
+                data-testid={ `${index}-horizontal-image` }
+              />
+              <p data-testid={ `${index}-horizontal-top-text` }>{ dr.category }</p>
+              <p data-testid={ `${index}-horizontal-name` }>{ dr.name }</p>
+              <p data-testid={ `${index}-horizontal-done-date` }>{ dr.doneDate }</p>
+              <div>
+                {
+                  dr.tags.map((tag) => (
+                    <p
+                      key={ tag }
+                      data-testid={ `${index}-${tag}-horizontal-tag` }
+                    >
+                      { tag }
+                    </p>
+                  ))
+                }
+              </div>
+              <button
+                type="button"
+                data-testid={ `${index}-horizontal-share-btn` }
+              >
+                Compartilhar
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+    </section>
   );
 }
 
