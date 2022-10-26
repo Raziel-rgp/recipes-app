2f85f0075ba3fd2d78d32d1b4ed37c0718179e16
André Melo <proandremelo@gmail.com>
2022-10-26 19:48:17 -0300

    Reqs 57, 58, 59 e 61

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { getItem } from '../services/LocalStorageFuncs';
 
 function Profile() {
+  const { push } = useHistory();
   return (
     <section>
       <Header />
+      <p data-testid="profile-email">{getItem('user').email}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <Footer />
     </section>
   );
